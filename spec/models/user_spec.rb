ebc84da43d345ff3fa80cddcab4e32aa1038c89b
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-12-02 10:23:17 +0000

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with valid attributes' do
-    user = User.new(name: 'John Doe', posts_counter: 0)
-    expect(user).to be_valid
+  subject { User.new(name: 'Tom') }
+  describe 'User Model Name validation tests' do
+    it 'should have name present' do
+      subject.name = 'Tom'
+      expect(subject).to be_valid
+    end
+
+    it 'should not have name field empty' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
   end
 
-  it 'is not valid without a name' do
-    user = User.new(posts_counter: 0)
-    expect(user).to_not be_valid
+  describe 'User Model method tests' do
+    it 'should return three recent posts' do
+      author = User.create(name: 'Harry')
+      post4 = Post.create(title: 'Title4', author: author)
+      post5 = Post.create(title: 'Title5', author: author)
+      post6 = Post.create(title: 'Title6', author: author)
+      expect(author.recent_posts).to eq([post6, post5, post4])
+    end
   end
 
-  it 'is not valid with a negative posts_counter' do
-    user = User.new(name: 'Alice', posts_counter: -1)
-    expect(user).to_not be_valid
-  end
+  describe 'User counter validation ' do
+    it 'posts_counter should be an integer' do
+      subject.posts_counter = 8
+      expect(subject).to be_valid
+    end
 
-  it 'returns the 3 most recent posts' do
-    user = User.create(name: 'Bob', posts_counter: 0)
-    older_post = user.posts.create(title: 'Old Post', text: 'This is an old post')
-    newer_post1 = user.posts.create(title: 'Newer Post 1', text: 'This is a newer post')
-    newer_post2 = user.posts.create(title: 'Newer Post 2', text: 'This is another newer post')
-
-    recent_posts = user.recent_posts
-
-    expect(recent_posts).to eq([newer_post2, newer_post1, older_post])
+    it 'posts_counter should be a positive number' do
+      subject.posts_counter = -8
+      expect(subject).to_not be_valid
+    end
   end
 end
